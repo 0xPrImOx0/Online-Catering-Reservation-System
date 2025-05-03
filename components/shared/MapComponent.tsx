fc9dfa08076426d0aff0f3c8ecfdbf5c453a345f
Rey Daug <daug.rey34@gmail.com>
2025-05-03 23:17:49 +0800

    refactor: ⚒️ Simplify MapComponent by removing Leaflet integration and using an embedded Google Maps link

--- components/shared/MapComponent.tsx
+++ components/shared/MapComponent.tsx
@@ -1,61 +1,32 @@
-"use client";
-import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-import markerIconPng from "leaflet/dist/images/marker-icon.png";
-import markerIcon2xPng from "leaflet/dist/images/marker-icon-2x.png";
-import markerShadowPng from "leaflet/dist/images/marker-shadow.png";
 import { businessMetadata } from "@/lib/caterer/business-metadata";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-// Fix marker icons
-delete (L.Icon.Default as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconUrl: markerIconPng,
-  iconRetinaUrl: markerIcon2xPng,
-  shadowUrl: markerShadowPng,
-});
-
-const { map, businessName } = businessMetadata;
+const { map } = businessMetadata;
 
 export default function MapComponent() {
-  const handleViewOnGoogleMaps = () => {
-    const googleMapsUrl = `https://www.google.com/maps?q=${map.latitude},${map.longitude}`;
-    window.open(googleMapsUrl, "_blank");
-  };
-
   return (
-    <div>
-      <MapContainer
-        className="h-[500px]"
-        center={[map.latitude, map.longitude]}
-        zoom={map.zoom}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[map.latitude, map.longitude]}>
-          <Popup>
-            <h2>{name}</h2>
-            {map.address}
-          </Popup>
-        </Marker>
-      </MapContainer>
-      <div className="flex flex-col items-center mt-8">
-        <Button asChild variant={"outline"}>
-          <Link
-            href={`https://www.google.com/maps?q=${map.latitude},${map.longitude}`}
-            target="_blank"
-            rel="nofollow noopener noreferrer"
-            className="text-justify text-wrap"
-          >
-            View on Google Maps
-          </Link>
-        </Button>
-      </div>
+    <div className="flex flex-col items-center gap-4">
+      <iframe
+        src={map.embeddedLink}
+        width="100%"
+        height="450"
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        className="rounded-xl border shadow-md w-full max-w-5xl"
+      ></iframe>
+
+      <Button asChild variant="outline">
+        <Link
+          href={map.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-center"
+        >
+          View on Google Maps
+        </Link>
+      </Button>
     </div>
   );
 }
--- lib/caterer/business-metadata.tsx
+++ lib/caterer/business-metadata.tsx
@@ -4,6 +4,8 @@ const businessMetadata = {
     "University of Science and Technology of Southern Philippines, Cagayan de Oro City, Philippines",
   map: {
     link: "https://maps.app.goo.gl/NVDdvaswMioboVp27",
+    embeddedLink:
+      "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1213.9020884663971!2d124.6563335!3d8.4852594!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32fff2c3ca5ae8c7%3A0x880805868ab84491!2sUniversity%20of%20Science%20and%20Technology%20of%20Southern%20Philippines%20-%20CDO%20Campus!5e1!3m2!1sen!2sph!4v1746285060356!5m2!1sen!2sph",
     zoom: 15,
     address:
       "University of Science and Technology of Southern Philippines, Cagayan de Oro City, Philippines",
